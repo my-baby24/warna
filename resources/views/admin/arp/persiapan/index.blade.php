bca9ab93573cb5a61b7655f72c6b0e0c77101e26
Ariezeh <karie09012002@gmail.com>
2023-09-15 15:25:18 +0700

    p info

--- resources/views/admin/arp/persiapan/index.blade.php
+++ resources/views/admin/arp/persiapan/index.blade.php
@@ -1,35 +1,12 @@
 @extends('layouts.admin')
 @section('content')
 
- <style>
-        .toggle-button {
-            display: inline-block;
-            background-color: #ccc;
-            padding: 5px 10px;
-            border-radius: 20px;
-            cursor: pointer;
-        }
-
-        input[type="radio"] {
-            display: none;
-        }
-
-        input[type="radio"]:checked + .toggle-button {
-            background-color: #4CAF50; /* Warna hijau untuk "True" */
-            color: white;
-        }
-
-        input[type="radio"]:not(:checked) + .toggle-button {
-            background-color: #FF5733; /* Warna merah untuk "False" */
-            color: white;
-        }
-    </style>
 <div class="row">
         <div class="col-12">
         
             <div class="card">
                 <div class="card-header">
-                    Informasi Pembelajaran
+                    Persiapan
                 </div>
                 <div class="card-body small">
                     <div class="table-responsive">
@@ -174,22 +151,6 @@
         </div>
     </div>
     
-
-<style>
-        .table-responsive {
-            overflow-x: auto;
-        }
-
-        .table {
-            width: 100%;
-            max-width: none;
-        }
-
-        .table th,
-        .table td {
-            white-space: nowrap;
-        }
-    </style>
 
     <script src="{{ asset('assets/vendors/simple-datatables/simple-datatables.js') }}"></script>
     <script>
