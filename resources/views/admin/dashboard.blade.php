b97ad66fd014ecb7fac376b6b4fc6b1ab8492165
Ariezeh <karie09012002@gmail.com>
2023-09-18 16:18:19 +0700

    n

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,10 +1,91 @@
 @extends('layouts.admin')
 @section('content')
 <div class="container d-felx">
-<h2>PLN CORPU BERTRANSFORMASI</h2>
+<h2>PLN CORPU BERTRANSFORMASI</h2><p>
 
+  <div class="page-content">
+                <section class="row">
+                    <div class="col-12 col-lg-9">
+                        <div class="row">
+                            <div class="col-6 col-lg-3 col-md-6">
+                                <div class="card">
+                                    <div class="card-body px-3 py-4-5">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="stats-icon purple">
+                                                    <i class="iconly-boldShow"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <h6 class="text-muted font-semibold">Total Check in</h6>
+                                                <h6 class="font-extrabold mb-0">67</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-lg-3 col-md-6">
+                                <div class="card">
+                                    <div class="card-body px-3 py-4-5">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="stats-icon blue">
+                                                    <i class="iconly-boldProfile"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <h6 class="text-muted font-semibold">Total Peserta</h6>
+                                                <h6 class="font-extrabold mb-0">3.200</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-lg-3 col-md-6">
+                                <div class="card">
+                                    <div class="card-body px-3 py-4-5">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="stats-icon green">
+                                                    <i class="iconly-boldAdd-User"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <h6 class="text-muted font-semibold">Following</h6>
+                                                <h6 class="font-extrabold mb-0">80.000</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-6 col-lg-3 col-md-6">
+                                <div class="card">
+                                    <div class="card-body px-3 py-4-5">
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <div class="stats-icon red">
+                                                    <i class="iconly-boldBookmark"></i>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <h6 class="text-muted font-semibold">Saved Post</h6>
+                                                <h6 class="font-extrabold mb-0">112</h6>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        
+                    </div>
+                    
+                    
+                </section>
+            </div>
 
-  <div style="text-align: center;">
+            <!-- Are untuk tampilan Layout -->
+             <!-- <div style="text-align: center;">
     <h4>Denah UPDL Padang</h4>
     <img src="{{asset('assets/denah/layoutupdl.png')}}" alt="" class="img-fluid" style="max-width: 100%; max-height: 100%; display: block; margin: 0 auto;">
   </div><p>
@@ -12,8 +93,5 @@
   <div style="text-align: center;">
     <h4>Denah UPDL Padang</h4>
     <img src="{{asset('assets/denah/colenco.png')}}" alt="" class="img-fluid" style="max-width: 100%; max-height: 100%; display: block; margin: 0 auto;">
-  </div>
-
-
-</div>
+  </div> -->
 @endsection
