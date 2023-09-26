9d8728929e896f52fb68ad486f44845dcbae2c17
Ariezeh <karie09012002@gmail.com>
2023-09-26 10:38:33 +0700

    l

--- resources/views/layouts/dashboard/about.blade.php
+++ resources/views/layouts/dashboard/about.blade.php
@@ -8,10 +8,10 @@
       <div class="container">
 
         <ol>
-          <li><a href="{{route('wlcm')}}">Home</a></li>
-          <li>About Us</li>
+          <li><a href="{{route('wlcm')}}">Beranda</a></li>
+          <li>Tentang Kami</li>
         </ol>
-        <h2>About Us</h2>
+        <h2>Tentang Kami</h2>
 
       </div>
     </section><!-- End Breadcrumbs -->
@@ -25,10 +25,10 @@
           <div>
             <iframe class="mb-4 mb-lg-0" src="https://www.youtube.com/embed/_pSJxARxImY?autoplay=1&mute=1" frameborder="0" style="border:0; width: 100%; height: 384px;" allowfullscreen></iframe>
           </div>
-            <a href="https://www.youtube.com/embed/_pSJxARxImY" class="watch-now-button">Watch Now</a>
+            <a href="https://www.youtube.com/embed/_pSJxARxImY" class="watch-now-button">Tonton Sekarang</a>
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 content">
-            <h3>Profile Perusahaan</h3>
+            <h3>Profil Perusahaan</h3>
             <h5><p class="fst-italic">
             VISI
             </p></h5>
