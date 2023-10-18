a9db14b4cefdc53a2b8210d601d90e7441569858
Ariezeh <karie09012002@gmail.com>
2023-10-18 08:18:20 +0700

    lll

--- resources/views/user/infosarapan.blade.php
+++ resources/views/user/infosarapan.blade.php
@@ -1,9 +1,104 @@
 @extends('layouts.appUser')
 @section('content')
-<!-- Are untuk tampilan Layout -->
-    <div style="text-align: center;">
-        <h4>Info Jam Sarapan</h4>
-        
-    </div><p>
+<style>
+    /* Gaya untuk tabel */
+    table {
+        width: 100%;
+        border-collapse: collapse;
+        margin-bottom: 20px;
+    }
 
-  @endsection
+    th, td {
+        padding: 15px;
+        border: 1px solid #ddd;
+        text-align: left;
+        font-size: 14px;
+    }
+
+    th {
+        background-color: #007bff;
+        color: #fff;
+        font-size: 16px;
+    }
+
+    /* Gaya untuk heading */
+    h4 {
+        text-align: center;
+        color: #007bff;
+        margin-bottom: 20px;
+        font-size: 22px;
+    }
+
+    /* Gaya untuk baris genap */
+    tr:nth-child(even) {
+        background-color: #f9f9f9;
+    }
+
+    /* Gaya saat hover */
+    tr:hover {
+        background-color: #e0e0e0;
+    }
+</style>
+
+<section id="basic-form-layouts">
+    <div class="row match-height">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Makan lah lu, Bia paruik Kanyang</h4>
+                </div>
+                <div class="card-content">
+                    <div class="card-body">
+                        
+<div>
+    <!-- Tabel 1: Jadwal Waktu Makan -->
+    <h4>Jadwal Waktu Makan</h4>
+    <table>
+        <tr>
+            <th>Makanan</th>
+            <th>Waktu</th>
+        </tr>
+        <tr>
+            <td>Makan Pagi</td>
+            <td>06:30 - 07:30</td>
+        </tr>
+        <tr>
+            <td>Makan Siang</td>
+            <td>12:00 - 13:30</td>
+        </tr>
+        <tr>
+            <td>Makan Malam</td>
+            <td>18:00 - 20:00</td>
+        </tr>
+    </table>
+
+    <!-- Tabel 2: Jadwal Waktu Coffee Break -->
+    <h4>Jadwal Waktu Coffee Break</h4>
+    <table>
+        <tr>
+            <th>Coffee Break</th>
+            <th>Waktu</th>
+        </tr>
+        <tr>
+            <td>Coffee Break Pagi</td>
+            <td>10:00 - 10:30</td>
+        </tr>
+        <tr>
+            <td>Coffee Break Sore</td>
+            <td>15:00 - 15:30</td>
+        </tr>
+    </table>
+</div>
+</div>
+                </div>
+            </div>
+
+
+        </div>
+    
+    
+    </div>
+</section>
+
+@endsection
+
