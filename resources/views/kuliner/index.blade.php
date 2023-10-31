e94a60a172493e84c21ed68d4d47ef75205772aa
Ariezeh <karie09012002@gmail.com>
2023-10-31 10:50:22 +0700

    halo gais

--- resources/views/kuliner/index.blade.php
+++ resources/views/kuliner/index.blade.php
@@ -3,56 +3,74 @@
 @section('content')
     <!-- ======= Featured Section ======= -->
     <section id="fitur" class="fitur">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-3">
-            <div class="icon-box">
-            <a href="{{ route('ip.index') }}">
-              <!-- <i class="bi bi-card-checklist"></i> -->
-              <img src="{{asset('assets/images/1.png')}}" alt="Icon 1" class="img-fluid mb-3" style="max-width: 60px;">
-              <h3 class="text-dark">Informasi Pembelajaran</h3>
-              <p>Lihat Informasi Pembelajaran Disini</p>
-              </a>
-            </div>
-          </div>
+    <div class="container">
+      
 
-          <div class="col-lg-3 mt-4 mt-lg-0">
-          <div class="icon-box">
-            <a href="{{route('soon.index')}}">
-              <img src="{{asset('assets/images/3.png')}}" alt="Icon 2" class="img-fluid mb-3" style="max-width: 60px;">
-              <h3 class="text-dark">Check-in Penginapan</h3>
-              <h1 style="font-size: 35px; color: #000000; font-weight: bold;">Coming Soon!</h1>
+      <div class="row">
+        <div class="col-lg-2">
+          <div class="icon-box text-center p-3">
+            <a href="{{ route('ip.index') }}">
+              <img src="{{ asset('kuliner/images/1.png')}}" alt="Icon 1" class="img-fluid mb-3" style="max-width: 100px;">
+              <h5 class="text-dark mb-0" style="font-size: 16px;">Wisata</h5>
+              <!-- <p class="text-muted" style="font-size: 14px;">Lihat Informasi Pembelajaran Disini</p> -->
             </a>
           </div>
         </div>
 
-          <div class="col-lg-3 mt-4 mt-lg-0">
-            <div class="icon-box">
-            <a href="{{route('sarana.index')}}">
-            <!-- <a href="{{route('soon.index')}}"> -->
-              <!-- <i class="bi bi-binoculars"></i> -->
-              <img src="{{asset('assets/images/4.png')}}" alt="Icon 3" class="img-fluid mb-3" style="max-width: 60px;">
-              <h3 class="text-dark">Sarana dan Prasarana</h3>
-              <p>Lihat Sarana & Prasarana Disini</p>
-              </a>
-            </div>
+        <div class="col-lg-2">
+          <div class="icon-box text-center p-3">
+            <a href="{{ route('ip.index') }}">
+              <img src="{{ asset('kuliner/images/2.png')}}" alt="Icon 1" class="img-fluid mb-3" style="max-width: 100px;">
+              <h5 class="text-dark mb-0" style="font-size: 16px;">Kuliner</h5>
+              <!-- <p class="text-muted" style="font-size: 14px;">Lihat Informasi Pembelajaran Disini</p> -->
+            </a>
           </div>
+        </div>
 
-          <div class="col-lg-3 mt-4 mt-lg-0">
-            <div class="icon-box">
-            <a href="{{ route('kuliner') }}">
-              <!-- <i class="bi bi-binoculars"></i> -->
-              <img src="{{asset('assets/images/5.png')}}" alt="Icon 4" class="img-fluid mb-3" style="max-width: 60px;">
-              <h3 class="text-dark">Kuliner, Wisata dan Transportasi</h3>
-              <p>Coba cari Makanan, Tempat, dan Transportasi di dekat Anda Disini</p>
+        <div class="col-lg-2">
+          <div class="icon-box text-center p-3">
+              <a href="{{ route('ip.index') }}">
+                  <img src="{{ asset('kuliner/images/3.png')}}" alt="Icon 1" class="img-fluid mb-3" style="max-width: 100px;">
+                  <h5 class="text-dark mb-0" style="font-size: 16px;">Kafe</h5>
+                  <!-- <p class="text-muted" style="font-size: 14px;">Lihat Informasi Pembelajaran Disini</p> -->
               </a>
-            </div>
           </div>
-          
+      </div>
+
+        <div class="col-lg-2">
+          <div class="icon-box text-center p-3">
+            <a href="{{route('soon.index')}}">
+              <img src="{{ asset('kuliner/images/4.png')}}" alt="Icon 2" class="img-fluid mb-3" style="max-width: 100px;">
+              <h5 class="text-dark mb-0" style="font-size: 16px;">Hotel</h5>
+              <!-- <h6 style="font-size: 18px; color: #000000; font-weight: bold;">Coming Soon!</h6> -->
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-2">
+          <div class="icon-box text-center p-3">
+            <a href="{{route('sarana.index')}}">
+              <img src="{{ asset('kuliner/images/5.png')}}" alt="Icon 3" class="img-fluid mb-3" style="max-width: 100px;">
+              <h5 class="text-dark mb-0" style="font-size: 16px;">Transportasi</h5>
+              <!-- <p class="text-muted" style="font-size: 14px;">Lihat Sarana & Prasarana Disini</p> -->
+            </a>
+          </div>
+        </div>
+
+        <div class="col-lg-2">
+          <div class="icon-box text-center p-3">
+            <a href="{{ route('kuliner') }}">
+              <img src="{{ asset('kuliner/images/6-1.png')}}" alt="Icon 4" class="img-fluid mb-3" style="max-width: 100px;">
+              <h5 class="text-dark mb-0" style="font-size: 16px;">Pusat Oleh-Oleh</h5>
+              <!-- <p class="text-muted" style="font-size: 14px;">Cari Kuliner Pilihan Terbaik di Sekitar Anda</p> -->
+            </a>
+          </div>
         </div>
 
       </div>
-    </section><!-- End Featured Section -->
+    </div>
+  </section>
+
   
 
 @endsection
