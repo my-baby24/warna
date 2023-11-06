52001ecdf58cf58bf314d8a34fee7f6e5f01a099
Ariezeh <karie09012002@gmail.com>
2023-11-06 15:38:55 +0700

    halool

--- resources/views/kwt/wisata/index.blade.php
+++ resources/views/kwt/wisata/index.blade.php
@@ -3,52 +3,163 @@
 
 @section('content')
         <div id="slide">
-            <div class="item" style="background-image: url('https://i.ibb.co/QYk7MG6/1.jpg');">
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/air terjun lubuk bonta/m11.jpg') }}');">
                 <div class="content">
-                    <div class="name">LUNDEV</div>
-                    <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-                        <div id="button-container">
-                          <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
-                          </button>
-                        </div>
+                    <div class="name">Air Terjun Lubuk Bonta</div>
+                    <div class="des">Eksplorasi Keajaiban Air Terjun Lubuk Bonta</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i>  Kayu Tanam, Kabupaten Padang Pariaman</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div class="item" style="background-image: url('https://i.ibb.co/QYk7MG6/1.jpg');">
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/danau maninjau/m.jpg') }}');">
                 <div class="content">
-                    <div class="name">LUNDEV</div>
-                    <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-                    <button>See more</button>
+                    <div class="name">Danau Maninjau</div>
+                    <div class="des">Keindahan Danau Maninjau yang Memikat Hati</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kec. Tanjung Raya, Kabupaten Agam</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div class="item" style="background-image: url(3.jpg);">
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/danau singkarak/m.jpg') }}');">
                 <div class="content">
-                    <div class="name">LUNDEV</div>
-                    <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-                    <button>See more</button>
+                    <div class="name">Danau Singkarak</div>
+                    <div class="des">Pesona Danau Singkarak yang Menakjubkan</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Perbatasan Solok dan Tanah Datar</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div class="item" style="background-image: url('https://i.ibb.co/QYk7MG6/1.jpg');">
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/Istano Basa Pagaruyung/m.jpg') }}');">
                 <div class="content">
-                    <div class="name">LUNDEV</div>
-                    <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-                    <button>See more</button>
+                    <div class="name">Istano Baso Pagaruyuang</div>
+                    <div class="des">Keagungan Istano Baso Pagaruyuang yang menyimpan Cerita Kuno</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kabupaten Tanah Datar</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div class="item" style="background-image: url(5.jpg);">
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/jam gadang/m.jpeg') }}');">
                 <div class="content">
-                    <div class="name">LUNDEV</div>
-                    <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-                    <button>See more</button>
+                    <div class="name">Jam Gadang</div>
+                    <div class="des"> Menyaksikan Waktu di Bawah Bayang Jam Gadang yang Megah.</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kota Bukittinggi</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
                 </div>
             </div>
-            <div class="item" style="background-image: url(6.jpg);">
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/kolam tirta alami/m.jpg') }}');">
                 <div class="content">
-                    <div class="name">LUNDEV</div>
-                    <div class="des">Tinh ru anh di chay pho, chua kip chay pho thi anhchay mat tieu</div>
-                    <button>See more</button>
+                    <div class="name">Kolam Tirta Alami</div>
+                    <div class="des">Merenungi Kesejukan di Kolam Tirta Alami yang Menyegarkan</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kec. 2 X 11 Kayu Tanam, Kabupaten Padang Pariaman</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/LEMBAH ANAI/m.jpg') }}');">
+                <div class="content">
+                    <div class="name">Lembah Anai</div>
+                    <div class="des">Menikmati Kesejukan di Lembah Anai yang Menyajikan Kecantikan Alam yang Damai</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kec. 2 X 11 Kayu Tanam, Kabupaten Padang Pariaman</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/masjid raya sumbar/m.jpg') }}');">
+                <div class="content">
+                    <div class="name">Masjid Raya Sumatera Barat</div>
+                    <div class="des">Mengagumi Keindahan Arsitektur Islam di Masjid Raya Sumatera Barat yang Penuh Makna Sejarah</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kec. 2 X 11 Kayu Tanam, Kabupaten Padang Pariaman</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/pantai air manis/m.jpg') }}');">
+                <div class="content">
+                    <div class="name">Pantai Air Manis</div>
+                    <div class="des">Menikmati Kelembutan Pasir dan Kesejukan Ombak</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kec. 2 X 11 Kayu Tanam, Kabupaten Padang Pariaman</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/pantai gundoriah/m.jpg') }}');">
+                <div class="content">
+                    <div class="name">Pantai gondariah</div>
+                    <div class="des">Merasakan Pesona Pantai Gondariah dengan Pasir Putih dan Pemandangan Laut yang Spektakuler</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kec. 2 X 11 Kayu Tanam, Kabupaten Padang Pariaman</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/pantai pasir jambak/m.jpg') }}');">
+                <div class="content">
+                    <div class="name">Pantai Pasir Jambak</div>
+                    <div class="des">Menghayati Keindahan Pantai Pasir Jambak dengan Suasana Romantis dan Pasir yang Lembut</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kec. 2 X 11 Kayu Tanam, Kabupaten Padang Pariaman</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/puncak lawang/m.jpg') }}');">
+                <div class="content">
+                    <div class="name">Puncak Lawang</div>
+                    <div class="des">Menyaksikan Keelokan Alam dari Ketinggian</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kec. 2 X 11 Kayu Tanam, Kabupaten Padang Pariaman</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div class="item" style="background-image: url('{{ asset('kuliner/images/DATA WISATA/Taman Wisata Lembah Harau/m.jpg') }}');">
+                <div class="content">
+                    <div class="name">Lembah Harau</div>
+                    <div class="des">Terpana oleh Keunikan Lembah Harau dengan Tebing Batu dan Air Terjun yang Menakjubkan</div>
+                    <div class="des"><i class="fas fa-map-marker-alt"></i> Kabupaten Limapuluh Koto, Payakumbuh</div>
+                    <div id="button-container">
+                        <button class="primary-button">Cek Selengkapnya !<span class="round"></span>
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
+
         <div class="buttons">
             <button id="prev"><i class="fa-solid fa-angle-left"></i></button>
             <button id="next"><i class="fa-solid fa-angle-right"></i></button>
